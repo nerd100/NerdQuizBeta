a19d90e5b7654faea45b16621401e0b2a58e005e
nerd100 <den.meyer1@web.de>
2016-10-03 17:57:55 +0200

    hope

--- app/src/main/java/com/example/dennis/nerdquiz/Score.java
+++ app/src/main/java/com/example/dennis/nerdquiz/Score.java
@@ -258,7 +258,7 @@ public class Score extends Activity {
     }
     public String getNerdTitle(int Score){              //r
         for(int i = 0; i < ranksValue.size(); i++){
-            if(Score < Integer.parseInt(ranksValue.get(i))){
+            if(Score <= Integer.parseInt(ranksValue.get(i))){
                 return ranksTitle.get(i);
             }
 
